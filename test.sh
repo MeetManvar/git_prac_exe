6474cc26af09b4e61243817103b90df3186213c4
Meet Manvar <60876390+MeetManvar@users.noreply.github.com>
2022-03-31 18:29:35 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
-
+docker login --username="meetmanvar" --password="Meet.docker@1234"
 var=$(docker ps -lq)
 docker stop $var 
 npm install
 docker build -t meetmanvar/cicdrepo:latest .
-docker login --username="meetmanvar" --password="Meet.docker@1234"
 docker push meetmanvar/cicdrepo:latest
 docker run -p 3000:3000 meetmanvar/cicdrepo:latest
