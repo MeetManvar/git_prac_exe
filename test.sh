b56a47c0144c566049c15b99150095e4b267cf4a
Meet Manvar <60876390+MeetManvar@users.noreply.github.com>
2022-03-31 18:23:02 +0530

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+var=$(docker ps -lq)
+docker stop $var 
+npm install
+docker build -t meetmanvar/cicdrepo:latest .
+docker login --username="meetmanvar" --password="Meet.docker@1234"
+docker push meetmanvar/cicdrepo:latest
+docker run -p 3000:3000 --name cicd meetmanvar/cicdrepo:latest
