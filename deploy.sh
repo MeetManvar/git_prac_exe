f5dd4810bc573cd2df62234e1e754723a2d5ad8c
Meet Manvar <60876390+MeetManvar@users.noreply.github.com>
2022-04-06 11:54:39 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ ssh ubuntu@54.242.33.225 << 'ENDSSH'
 docker login --username "meetmanvar" --password "Meet.docker@1234"
 docker stop jenkins
 docker rm jenkins
-docker rmi meetmanvar/cicdrepo:latest
-docker run --name jenkins -p 3001:3000 meetmanvar/cicdrepo:latest
+docker rmi meetmanvar/cicdrepo:$1
+docker run --name jenkins -p 3001:3000 meetmanvar/cicdrepo:$1
 ENDSSH
